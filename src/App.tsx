f4512bd5ce3ccd937f47b1649c63d212d68d7f9b
Khabibullo Abduvahidov <khabibullo@vakhid.digital>
2022-02-01 12:35:23 +0500

    user component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Button from "./components/button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
-          <Button>Hello world</Button>
+
       </header>
     </div>
   );
