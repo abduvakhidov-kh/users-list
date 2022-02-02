b14b0372849aaf74276869e47bb08813fc07c7c7
Khabibullo Abduvahidov <khabibullo@vakhid.digital>
2022-02-02 16:39:32 +0500

    user component completed

--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -1,10 +1,27 @@
 import {FC} from "react";
+import { Link } from "react-router-dom";
+import "./user.style.scss";
 
+interface props {
+    fullName: string;
+    city: string;
+    company: string;
+    id: number;
+}
 
-const User: FC = () => {
+const User: FC<props> = ({fullName, city, company, id}) => {
 
     return(
-        <div>User</div>
+        <div className="user">
+            <ul className="user__data">
+                <li>ФИО: <span>{fullName}</span></li>
+                <li>город: <span>{city}</span></li>
+                <li>компания: <span>{company}</span></li>
+            </ul>
+            <Link to={"/user/" + id}>
+                <span  className="user_about user__link ">Подробнее</span>
+            </Link>
+        </div>
     );
 };
 
