e8a997191bf840d244e5cdf556e06dde58c7a9e6
Khabibullo Abduvahidov <khabibullo@vakhid.digital>
2022-02-02 16:42:00 +0500

    add routing and creat main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import {useEffect, FC, useState} from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import UserList from "./components/users-list/users-list";
+import Sidebar from "./components/sidebar/sidebar";
+import "./App.style.scss";
+import UserProfile from "./page/user-profile";
 
-function App() {
+const App: FC = () => {
+    const [users, setUsers] = useState<Array<object>>([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                return response.json();
+            })
+            .then((data) => {
+                setUsers( [...data] );
+            })
+            .catch(error => console.log("error", error))
+    }, [])
+
+    console.log(users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Sidebar/>
+        <Routes>
+          <Route path="/" element={<UserList userList={users} />} />
+        </Routes>
+        <Routes>
+          <Route path={"/user/:id"} element={<UserProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
